fb6afe5b44fe0d0ac6c8e6f15d3381337a530296
OussemaBenAmeur <oussemabenameur9@gmail.com>
2025-06-20 13:37:40 +0100

    mod

--- Word_Frequency_Counter.java
+++ Word_Frequency_Counter.java
@@ -1,19 +1,16 @@
 import java.io.*;
 import java.util.*;
 
-public class WordFrequencyCounter {
+public class Main {
     public static void main(String[] args) {
 
-        String fileName = "input.txt";
+        String fileName = "Variables.txt";
         Map<String, Integer> wordFreq = new HashMap<>();
-
         try {
             FileReader fr = new FileReader(fileName);
             BufferedReader br = new BufferedReader(fr);
             String line;
-
             while ((line = br.readLine()) != null) {
-                // Remove punctuation and convert to lowercase
                 line = line.replaceAll("[^a-zA-Z ]", "").toLowerCase();
                 String[] words = line.split("\\s+");
 
@@ -23,21 +20,16 @@ public class WordFrequencyCounter {
                     }
                 }
             }
-
             br.close();
             fr.close();
-
             List<Map.Entry<String, Integer>> sortedWords = new ArrayList<>(wordFreq.entrySet());
             sortedWords.sort((a, b) -> {
                 int freqCompare = Integer.compare(b.getValue(), a.getValue());
                 return freqCompare != 0 ? freqCompare : a.getKey().compareTo(b.getKey());
             });
-
-
             for (Map.Entry<String, Integer> entry : sortedWords) {
                 System.out.println(entry.getKey() + ": " + entry.getValue());
             }
-
         } catch (IOException e) {
             System.out.println("Error reading file: " + e.getMessage());
         }
